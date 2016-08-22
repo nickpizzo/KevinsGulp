954d4a4805296c3447437fddfa22da0c4675508e
Nick Pizzo <nick.pizzo@gmail.com>
2016-08-22 11:42:14 -0700

    Add support for images

--- gulpfile.js
+++ gulpfile.js
@@ -40,11 +40,19 @@ gulp.task('styles', function () {
   }));
 });
 
+gulp.task('images', () => {
+  gulp.src('src/**/*.{png,jpg,svg}')
+    .pipe(gulp.dest('dist'))
+    .pipe(sync.reload({
+      stream: true
+    }));
+});
+
 ////////////////////////////////////////////////////
 //PIPES SRC FILES INTO DIST & WATCHES CHANGES IN SRC
 ///////////////////////////////////////////////////
 
-gulp.task('build', ['html', 'scripts', 'styles']);
+gulp.task('build', ['html', 'scripts', 'styles', 'images']);
 
 gulp.task('serve', ['build'], function () {
   sync.init({
@@ -54,6 +62,7 @@ gulp.task('serve', ['build'], function () {
   gulp.watch('src/*.{html,jade}', ['html']);
   gulp.watch('src/*.js', ['scripts']);
   gulp.watch('src/*.{css,scss,sass}', ['styles']);
+  gulp.watch('src/**/*.{png,jpg,svg}', ['images']);
 });
 
 //////////////////////////////
